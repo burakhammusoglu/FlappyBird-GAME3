d1df46b3add2f8b46421c0011625a6e7bf6c5357
burakhammusoglu <mburak.hammusoglu@gmail.com>
2020-03-12 16:23:43 +0300

    FlappyBird.v

---
+++ Assets/birdCode.cs
@@ -0,0 +1,94 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class birdCode : MonoBehaviour
+{
+    public Sprite[] birdSprite;
+    SpriteRenderer spriteRenderer;
+    bool nextBackControl = true;
+    float birdAnimationTime = 0;
+    int birdCounter;
+    Rigidbody2D physics;
+    int puan = 0;
+    bool gameOver = true;
+
+    public Text soccerText;
+    gameController gameControl;
+    void Start()
+    {
+        spriteRenderer = GetComponent<SpriteRenderer>();
+        physics = GetComponent<Rigidbody2D>();
+        gameControl = GameObject.FindGameObjectWithTag("gamecontroltag").GetComponent<gameController>();
+    }
+
+    
+    void Update()
+    {
+        birdController();
+        Animasyon();
+    }
+    void Animasyon()
+    {
+        birdAnimationTime += Time.deltaTime;
+        if (birdAnimationTime>0.3f)
+        {
+            birdAnimationTime = 0;
+            if (nextBackControl)
+            {
+                spriteRenderer.sprite = birdSprite[birdCounter];
+                birdCounter++;
+                if (birdCounter == birdSprite.Length)
+                {
+                    birdCounter--;
+                    nextBackControl = false;
+                }
+            }
+            else
+            {
+                birdCounter--;
+                spriteRenderer.sprite = birdSprite[birdCounter];
+                if (birdCounter==0)
+                {
+                    birdCounter++;
+                    nextBackControl = true;
+                }
+            }
+        }
+        
+
+    }
+    void birdController()
+    {
+        if (Input.GetMouseButtonDown(0) && gameOver)
+        {
+            physics.velocity = new Vector2(0, 0); // hiz(yerçekimi)0 yaptık
+            physics.AddForce(new Vector2(0, 200));//sonra kuvvet uyguladık
+        }
+        if (physics.velocity.y > 0)
+        {
+            transform.eulerAngles = new Vector3(0, 0, 45); // zıplarken yukarı bakmasını yaptık
+        }
+        else
+        {
+            transform.eulerAngles = new Vector3(0, 0, -45); // düşerkende aşağıya bakmasını sağladık
+        }
+    }
+
+    private void OnTriggerEnter2D(Collider2D col)
+    {
+        if (col.gameObject.tag=="puantag")
+        {
+            puan++;
+            soccerText.text = "puan=" + puan;
+            Debug.Log(puan);
+        }
+        if (col.gameObject.tag== "engeltag")
+        {
+            gameOver = false;
+            gameControl.gameOver();
+        }
+    }
+
+}
---
+++ Assets/gameController.cs
@@ -0,0 +1,81 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class gameController : MonoBehaviour
+{
+    public GameObject skyOne;
+    public GameObject skyTwo;
+    public float backRoundSpeed = -1.5f;
+    Rigidbody2D physicsOne;
+    Rigidbody2D physicsTwo;
+    float uzunluk = 0;
+
+
+    public GameObject engel;
+    public int kacAdetEngel = 0;
+    public GameObject[] engeller;
+
+    float degisimZaman = 0;
+    int sayac = 0;
+    void Start()
+    {
+        physicsOne = skyOne.GetComponent<Rigidbody2D>();
+        physicsTwo = skyTwo.GetComponent<Rigidbody2D>();
+
+        physicsOne.velocity = new Vector2(backRoundSpeed, 0);
+        physicsTwo.velocity = new Vector2(backRoundSpeed, 0);
+
+        uzunluk = skyOne.GetComponent<BoxCollider2D>().size.x;
+
+        engeller = new GameObject[kacAdetEngel];
+        for(int i = 0 ; i<engeller.Length; i++)
+        {
+            engeller[i] = Instantiate(engel, new Vector2(-20,-20),Quaternion.identity);
+            Rigidbody2D fizik =engeller[i].AddComponent<Rigidbody2D>();
+            fizik.gravityScale = 0;
+            fizik.velocity = new Vector2(backRoundSpeed, 0);
+        }
+        
+    }
+
+    
+    void Update()
+    {
+        if (skyOne.transform.position.x <= -uzunluk)
+        {
+            skyOne.transform.position += new Vector3(uzunluk * 2, 0);
+        }
+        if (skyTwo.transform.position.x <= -uzunluk)
+        {
+            skyTwo.transform.position += new Vector3(uzunluk*2,0);
+        }
+
+        //------------------------------------------------//
+
+        degisimZaman += Time.deltaTime;
+        if (degisimZaman > 2f)
+        {
+            degisimZaman = 0f;
+            float yPosition = Random.Range(-2f, 0.50f);
+            engeller[sayac].transform.position = new Vector3(15f, yPosition);
+            sayac++;
+            if (sayac >= engeller.Length)
+            {
+                sayac = 0;
+            }
+        
+        }
+    }
+
+    public void gameOver()
+    {
+        for(int i = 0; i <engeller.Length; i++)
+        {
+            engeller[i].GetComponent<Rigidbody2D>().velocity =Vector2.zero;
+            physicsOne.velocity = Vector2.zero;
+            physicsTwo.velocity = Vector2.zero;
+
+        }
+    }
+}
